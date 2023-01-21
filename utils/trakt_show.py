39578682dcd421f66e26f4c405dc1422dcb07eda
Jonas Lundberg <lundbergjonas96@gmail.com>
2023-01-21 15:57:06 +0000

    Fix 404 when scrobbling shows to trakt

--- utils/trakt_show.py
+++ utils/trakt_show.py
@@ -3,11 +3,11 @@ from utils.trakt_item import TraktItem
 
 class TraktShow(TraktItem):
     def __init__(self, m: int):
-        self.tmdb_id = m
+        self.tvdb_id = m
 
     def generateEpisodeTraktDict(self) -> dict:
         return {
             'ids': {
-                'tvdb': self.tmdb_id,
+                'tvdb': self.tvdb_id,
             }
         }
